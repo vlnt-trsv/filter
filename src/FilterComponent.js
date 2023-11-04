9cd4728660a456c8e5fcd147414d3ba6025498cc
Valentin <111338107+vlnt-trsv@users.noreply.github.com>
2023-11-04 21:53:04 +0700

    Changing the code structure
    Optimization, added doctor's data, etc.
    
    Co-Authored-By: armoney7 <112188406+armoney7@users.noreply.github.com>

--- src/FilterComponent.js
+++ src/FilterComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import doctorsData from "./DoctorsData";
+import "./select.css";
 
 class FilterComponent extends Component {
   constructor(props) {
@@ -27,16 +28,22 @@ class FilterComponent extends Component {
       admissionType,
     } = this.state;
 
+    // Параметры филтрации
+
     const filteredDoctors = doctorsData.filter((doctor) => {
       return (
-        (experience === "" || doctor.experience === experience) &&
-        (gender === "" || doctor.gender === gender) &&
-        (category === "" || doctor.category === category) &&
-        (academicDegree === "" || doctor.academicDegree === academicDegree) &&
-        (rating === "" || doctor.rating >= parseFloat(rating)) &&
-        (numberOfReviews === "" ||
-          doctor.numberOfReviews >= parseInt(numberOfReviews)) &&
-        (admissionType === "" || doctor.admissionType === admissionType)
+        (experience === "Все" || doctor.experience === experience) && // Фильтр по стажу
+        (gender === "" || doctor.gender === gender) && // Фильтр по полу
+        (category === "" || doctor.category === category) && // Фильтр по категории
+        (academicDegree === "" || doctor.academicDegree === academicDegree) && // Фильтр по ученой степени
+        (rating === "" || doctor.rating >= parseFloat(rating)) && // Фильтр по оценке
+        (numberOfReviews === "" || // Фильтр по количеству отзывов
+          (numberOfReviews === "Меньше 50" && doctor.numberOfReviews < 50) ||
+          (numberOfReviews === "50-100" &&
+            doctor.numberOfReviews >= 50 &&
+            doctor.numberOfReviews <= 100) ||
+          (numberOfReviews === "Больше 100" && doctor.numberOfReviews > 100)) &&
+        (admissionType === "" || doctor.admissionType === admissionType) // Фильтр по типу приема
       );
     });
 
@@ -47,128 +54,77 @@ class FilterComponent extends Component {
     this.filterDoctors();
   };
 
-  // Добавьте обработчики событий для изменения параметров фильтрации
-
   render() {
+    const commonOptions = {
+      experience: [
+        "Все",
+        "Не более 5 лет",
+        "От 5 до 10 лет",
+        "От 10 до 20 лет",
+        "Свыше 20 лет",
+      ],
+      gender: ["", "Мужской", "Женский"],
+      category: ["", "Первая", "Вторая", "Высшая"],
+      academicDegree: ["", "Бакалавр", "Магистр", "Доктор наук"],
+      rating: ["", "4.5", "4"],
+      numberOfReviews: ["", "Меньше 50", "50-100", "Больше 100"],
+      admissionType: ["", "Очное", "Заочное", "Дистанционное"],
+    };
+
+    const countFilteredDoctors = () => {
+      return this.state.filteredDoctors.length;
+    };
+
     return (
-      <div>
-        <label>
-          Стаж:
-          <select
-            value={this.state.experience}
-            onChange={(e) => this.setState({ experience: e.target.value })}
-          >
-            <option value="">Любой</option>
-            <option value="1 год">1 год</option>
-            <option value="2 года">2 года</option>
-            {/* Добавьте другие варианты стажа */}
-          </select>
-        </label>
-
-        <label>
-          Пол:
-          <input
-            type="radio"
-            name="gender"
-            value="Мужской"
-            checked={this.state.gender === "Мужской"}
-            onChange={(e) => this.setState({ gender: e.target.value })}
-          />{" "}
-          Мужской
-          <input
-            type="radio"
-            name="gender"
-            value="Женский"
-            checked={this.state.gender === "Женский"}
-            onChange={(e) => this.setState({ gender: e.target.value })}
-          />{" "}
-          Женский
-        </label>
-
-        <label>
-          Категория:
-          <select
-            value={this.state.category}
-            onChange={(e) => this.setState({ category: e.target.value })}
-          >
-            <option value="">Любая</option>
-            <option value="Первая">Первая</option>
-            <option value="Вторая">Вторая</option>
-            <option value="Высшая">Высшая</option>
-            {/* Добавьте другие варианты категории */}
-          </select>
-        </label>
-
-        <label>
-          Ученая степень:
-          <select
-            value={this.state.degree}
-            onChange={(e) => this.setState({ academicDegree: e.target.value })}
-          >
-            <option value="">Любая</option>
-            <option value="Бакалавр">Бакалавр</option>
-            <option value="Магистр">Магистр</option>
-            <option value="Доктор наук">Доктор наук</option>
-            {/* Добавьте другие варианты ученой степени */}
-          </select>
-        </label>
-
-        <label>
-          Оценка:
-          <select
-            value={this.state.rating}
-            onChange={(e) => this.setState({ rating: e.target.value })}
-          >
-            <option value="">Любая</option>
-            <option value="5 звезд">5 звезд</option>
-            <option value="4 звезды">4 звезды</option>
-            <option value="3 звезды">3 звезды</option>
-            {/* Добавьте другие варианты оценки */}
-          </select>
-        </label>
-
-        <label>
-          Количество отзывов:
-          <select
-            value={this.state.reviews}
-            onChange={(e) => this.setState({ numberOfReviews: e.target.value })}
-          >
-            <option value="">Любое</option>
-            <option value="Меньше 50">Меньше 50</option>
-            <option value="50-100">50-100</option>
-            <option value="Больше 100">Больше 100</option>
-            {/* Добавьте другие варианты количества отзывов */}
-          </select>
-        </label>
-
-        <label>
-          Тип приема:
-          <select
-            value={this.state.type}
-            onChange={(e) => this.setState({ admissionType: e.target.value })}
-          >
-            <option value="">Любой</option>
-            <option value="Очное">Очное</option>
-            <option value="Заочное">Заочное</option>
-            <option value="Дистанционное">Дистанционное</option>
-            {/* Добавьте другие варианты типа приема */}
-          </select>
-        </label>
-
-        <button onClick={this.handleFilterChange}>Применить фильтр</button>
+      <div className="container">
+        <div className="wrapper">
+          {Object.keys(commonOptions).map((key) => (
+            <label key={key}>
+              {key === "experience"
+                ? "Стаж:"
+                : key === "gender"
+                ? "Пол:"
+                : key === "category"
+                ? "Категория:"
+                : key === "academicDegree"
+                ? "Ученая степень:"
+                : key === "rating"
+                ? "Оценка:"
+                : key === "numberOfReviews"
+                ? "Количество отзывов:"
+                : "Тип приёма"}
+              <select
+                value={this.state[key]}
+                onChange={(e) => this.setState({ [key]: e.target.value })}
+              >
+                {commonOptions[key].map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </label>
+          ))}
+          <button type="button" onClick={this.handleFilterChange}>
+            Применить фильтр
+          </button>
+        </div>
 
         {/* Отображение результатов фильтрации */}
-        <div>
+        <div className="card">
+          <h1>Всего врачей: {countFilteredDoctors()}</h1>
           {this.state.filteredDoctors.map((doctor) => (
-            <div key={doctor.id}>
-              <p>Имя: {doctor.name}</p>
-              <p>Стаж: {doctor.experience}</p>
-              <p>Пол: {doctor.gender}</p>
-              <p>Категория: {doctor.category}</p>
-              <p>Ученая степень: {doctor.academicDegree}</p>
-              <p>Оценка: {doctor.rating}</p>
-              <p>Количество отзывов: {doctor.numberOfReviews}</p>
-              <p>Тип приема: {doctor.admissionType}</p>
+            <div className="card2" key={doctor.id}>
+              <div>
+                <p>Имя: {doctor.name}</p>
+                <p>Стаж: {doctor.experience}</p>
+                <p>Пол: {doctor.gender}</p>
+                <p>Категория: {doctor.category}</p>
+                <p>Ученая степень: {doctor.academicDegree}</p>
+                <p>Оценка: {doctor.rating}</p>
+                <p>Количество отзывов: {doctor.numberOfReviews}</p>
+                <p>Тип приема: {doctor.admissionType}</p>
+              </div>
             </div>
           ))}
         </div>
